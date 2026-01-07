7479486ce1c258ede65d8bc371922cdfe36b07be
Taigours Group of Organizations <taigoursgroup@gmail.com>
2026-01-07 13:48:38 +0545

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const supabase = createClient(
 
 // --- Express Setup ---
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.use(cors());
 app.use(bodyParser.json());
 
